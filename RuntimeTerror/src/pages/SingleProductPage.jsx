3b0ccec889fb8ab04a18bff41d1680bb63d1f07e
Prathana Ray <prathanaray@gmail.com>
2023-08-27 21:45:19 +0530

    updated SingleProductPage

--- RuntimeTerror/src/pages/SingleProductPage.jsx
+++ RuntimeTerror/src/pages/SingleProductPage.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React ,{useState}from "react";
+import { useParams } from "react";
+import {Card, CardFooter, Image, Button} from "@nextui-org/react";
+const SingleProductPage=()=>{
+  const[data,setData]=useState({})
+  const {id}=useParams();
+  console.log("x",id);
+  const api="";
+  async function fetchData(){
 
-const SingleProductPage = () => {
-  return (
-    <div>SingleProductPage</div>
-  )
+    let res= await fetchData(`${api}/${id}`)
+    let data1=await res.json();
+    console.log(data1.data,"data");
+    setData(data1.data,"data")
+
+  }
 }
-
-export default SingleProductPage
+fetchData() 
+  return (
+    <Box>
+    <Card
+      isFooterBlurred
+      radius="lg"
+      className="border-none"
+    >
+     <Image src={data.img} borderRadius="lg" />
+      <Heading size="md">{data.brand}</Heading>
+      <Text>{data.details}</Text>
+      <Text color="blue.600" fontSize="2xl">
+              {data.price}
+            </Text>
+      <CardFooter>
+          <ButtonGroup spacing="2">
+            <Button variant="solid" colorScheme="blue">
+              Buy now
+            </Button>
+            {/* <Button onClick={nav} variant="ghost" colorScheme="blue">
+              Details
+            </Button> */}
+          </ButtonGroup>
+        </CardFooter>
+    </Card>
+    </Box>
+  );
