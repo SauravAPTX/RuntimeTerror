4034d73f25ff39cf67aaf13ad58d67c0f6c4bb04
SauravAPTX <therealsauravmohanty@gmail.com>
2023-08-30 09:05:42 +0530

    added

--- RuntimeTerror/src/pages/Homepage.jsx
+++ RuntimeTerror/src/pages/Homepage.jsx
@@ -4,10 +4,14 @@ import NavbarComponent from './NavbarComponent';
 
 const Homepage=() =>{
     return(
-        <div>
-            <h1>home</h1>
-        <NavbarComponent/>
-        </div>
+        <div className="home">
+      <h1>Welcome to Our Store</h1>
+      <div className="product-list">
+        {products.map((product) => (
+          <Product key={product.id} product={product} onAddToCart={addToCart} />
+        ))}
+      </div>
+    </div>
     );
 }
 export default Homepage;
