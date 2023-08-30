cbf2e1a92110fa49baf3443ab0282df595bb36fc
Bhabna Mohanty <bhabnamohanty04@gmail.com>
2023-08-30 19:41:33 +0530

    changes

--- RuntimeTerror/src/pages/Homepage.jsx
+++ RuntimeTerror/src/pages/Homepage.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import NavbarComponent from './NavbarComponent';
 import Footer from './Footer';
 import { Avatar } from '@nextui-org/react';
-import CarouselSlider from '../components/CarouselSlider';
 
 
 const Homepage=() =>{
