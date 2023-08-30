aa5ee9b2614293ec49190e8742698d0b21d41fe4
SauravAPTX <therealsauravmohanty@gmail.com>
2023-08-30 21:07:41 +0530

    added cart features and removed errors

--- RuntimeTerror/src/components/SingleProduct.jsx
+++ RuntimeTerror/src/components/SingleProduct.jsx
@@ -9,6 +9,21 @@ export const SingleProduct = (props) => {
     function nav() {
         navigate(`/products/${id}`)
     }
+    const handleAddToCart = async (event) => {
+      event.preventDefault();
+      try {
+        await fetch("http://localhost:3000/cart", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ productId: id }),
+        });
+        console.log("Added to cart");
+      } catch (error) {
+        console.error("Error adding to cart:", error);
+      }
+    };
   return (
     <Card className="py-4" style={{ width: '30%', minWidth: '300px', marginRight: '2%' }}>
       <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
@@ -24,7 +39,8 @@ export const SingleProduct = (props) => {
           width={270}
         />
         <small className="text-default-500">{description}</small>
-        <button onClick={()=>nav()}className='btn btn-primary'>Buy Now!</button>
+        <button onClick={nav}className='btn btn-primary'>Details</button>
+        <button onClick={handleAddToCart}className='btn btn-primary'>Add to Cart</button>
        
       </CardBody>
     </Card>
