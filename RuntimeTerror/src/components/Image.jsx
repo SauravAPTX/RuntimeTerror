cb9805548635f489d904ac2d59f0a326d93ef464
Bhabna Mohanty <bhabnamohanty04@gmail.com>
2023-08-31 00:38:11 +0530

    Imagepage

---
+++ RuntimeTerror/src/components/Image.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Image} from "@nextui-org/react";
+
+export default function Imagepage() {
+  return (
+    <Image
+      width={1200}
+      alt="NextUI hero Image"
+      src="https://img.freepik.com/free-vector/flat-minimal-boutique-social-media-post-template_23-2149333998.jpg?size=626&ext=jpg&ga=GA1.1.291539164.1693422185&semt=ais"
+      align="center"
+    />
+  );
+}
--- RuntimeTerror/src/pages/Homepage.jsx
+++ RuntimeTerror/src/pages/Homepage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Avatars from '../components/Avatar';
 import Carousel from './Carousel';
+import Imagepage from '../components/Image';
 
 
 const Homepage=() =>{
     return(
         <div>
+          <Imagepage/>
           <Carousel/>
           <Avatars/>  
         </div>
